b0b497e8db08e820ded7ba8ee8c34f42c209fcaf
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-09-19 14:21:46 +0700

    check for summetry in breath first search

--- BreadthFirst/BreadthFirst.java
+++ BreadthFirst/BreadthFirst.java
@@ -1,5 +1,6 @@
 import java.util.Queue;
 import java.util.LinkedList;
+// CHECK FOR SYMMETRY!
 public class BreadthFirst {
   public static void main(String[] z){
     // Symmetric;
@@ -12,7 +13,7 @@ public class BreadthFirst {
     root.left.right = new Node();
     root.left.right.key = 3;
     root.left.left.left = new Node();
-    root.left.left.left.key = 4;
+    root.left.left.left.key = 5;
     root.left.right.right = new Node();
     root.left.right.right.key = 4;
     root.right = new Node();
@@ -24,31 +25,53 @@ public class BreadthFirst {
     root.right.left.left = new Node();
     root.right.left.left.key = 4;
     root.right.right.right = new Node();
-    root.right.right.right.key = 4;
-    System.out.println("Breadth First Search");
-    printBreadth(root);
+    root.right.right.right.key = 5;
+    System.out.println("Breadth First Search : Check Symmetry");
+    System.out.println(printBreadth(root.left,true));
     System.out.println();
-    System.out.println("Depth First Search");
-    printDepth(root);
+    System.out.println(printBreadth(root.right,false));
+    System.out.println();
+    System.out.println("Depth First Search : Check Symmetry");
+    // Check Symmetry by printing left & right 
+    System.out.println(printDepth(root.left,""));
+    System.out.println();
+    System.out.println(printDepthRightFirst(root,""));
   }
-  public static void printBreadth(Node root){
+  public static String printBreadth(Node root,boolean leftSide){
     Queue<Node> queue = new LinkedList<Node>();
     queue.add(root);
+    String str = "";
     while(!queue.isEmpty()){
       Node tempHead = queue.poll();
-      System.out.print(tempHead.key + " ");
-      if(tempHead.left != null){queue.add(tempHead.left);}
-      if(tempHead.right != null){queue.add(tempHead.right);}
+      // System.out.print(tempHead.key + " ");
+      str += tempHead.key;
+      if(leftSide){
+        if(tempHead.left != null){queue.add(tempHead.left);}
+        if(tempHead.right != null){queue.add(tempHead.right);}
+      } else{
+        if(tempHead.right != null){queue.add(tempHead.right);}
+        if(tempHead.left != null){queue.add(tempHead.left);}
+      }
     }
+    return str;
   }
-  public static void printDepth(Node root){
-    if(root == null){return;}
+  public static String printDepth(Node root,String str){
+    if(root == null){return "";}
+    str += root.key;
     System.out.print(root.key + " ");
-    printDepth(root.left);
-    printDepth(root.right);
+    String A = printDepth(root.left,str+root.key);
+    String B = printDepth(root.right,str+root.key);
+    return str;
   }
-  // Check Symmetry => If left and right don't have the same number
-  // using depth first search then it is not symmetrical
+  public static String printDepthRightFirst(Node root,String str){
+    if(root == null){return "";}
+    // System.out.print(root.key + " ");
+    if(root.left == null && root.right == null){return str;}
+    String A = printDepthRightFirst(root.right,str+root.key);
+    String B = printDepthRightFirst(root.left,str+root.key);
+    return A+B;
+  }
+
 }
 class Node{
   int key;
